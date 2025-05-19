4a3ae91795fd22c1c0f2ade2581a378a79e7e50f
joniaco0 <jiacovac@umd.edu>
2025-05-18 21:42:02 -0400

    Add files via upload

---
+++ RCleaning.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(fastDummies)
+baseball <- read.csv("https://raw.githubusercontent.com/joniaco0/jour479x_fall_2024/refs/heads/main/data/WARdata.csv")
+standings <- read.csv("https://raw.githubusercontent.com/joniaco0/jour479x_fall_2024/refs/heads/main/data/MLB_Data_with_Abbreviations%20-%20MLB_Data_with_Abbreviations.csv")
+standings <- standings |> rename(Team = Tm)
+baseball$Season <- as.integer(baseball$Season)
+merged_data <- baseball |>
+  left_join(standings, by = c("Team", "Season"))
+merged_data
+merged_data$WAR <- as.numeric(merged_data$WAR)
+corrections <- merged_data |>
+  mutate(Team = case_when(
+    Team %in% c("FLA", "MIA") ~ "MIA",
+    Team %in% c("ANA", "LAA") ~ "LAA",
+    Team %in% c("TBR", "TBD") ~ "TBR",
+    Team %in% c("WSN", "MON") ~ "WSN",
+    TRUE ~ Team # Keep other teams unchanged
+  ))
+maindata <- corrections |> select(Season, Name, PA, Team, WAR, pythW,pythL) |> mutate(WARSHARE = WAR/pythW)
+maindata <- maindata %>%
+  distinct(Team, Season, Name, .keep_all = TRUE)
